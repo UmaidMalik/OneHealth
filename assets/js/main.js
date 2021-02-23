c1a75fc799cf8f4df0a729e760b153fbfdfdc658
Umaid <umaid54321@hotmail.com>
2021-02-23 04:24:39 -0500

    i sleep................. zzZZZZ  :()

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,5 +113,5 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "X";
+  modal.style.display = "none";
 }
